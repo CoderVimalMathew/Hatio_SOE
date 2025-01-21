d78c78ce586f92795c86c791a5450cf340704d4c
CoderVimalMathew <coderv21@gmail.com>
2025-01-21 17:34:23 +0530

    Update HatioChallengeApplication.java

--- src/main/java/com/example/demo/HatioChallengeApplication.java
+++ src/main/java/com/example/demo/HatioChallengeApplication.java
@@ -3,11 +3,15 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+This module is used as the main file to initiate the spring-boot application.
+*/
 @SpringBootApplication
 public class HatioChallengeApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(HatioChallengeApplication.class, args);
-	}
-
+  /**
+  This is the main class where the spring application starts running.
+  */
+  public static void main(String[] args) {
+    SpringApplication.run(HatioChallengeApplication.class, args);
+  }
 }
