0fc1e7827dd5d2b85c4648e73e90b7ce10a52d5d
CoderVimalMathew <coderv21@gmail.com>
2025-01-21 17:14:51 +0530

    Update ConversionRequest.java

--- src/main/java/com/example/demo/model/ConversionRequest.java
+++ src/main/java/com/example/demo/model/ConversionRequest.java
@@ -1,31 +1,35 @@
 package com.example.demo.model;
 
+/**
+This module is used to get and set 
+the required attributes for a conversion.
+*/
 public class ConversionRequest {
-    private String from;
-    private String to;
-    private double amount;
+  private String from;
+  private String to;
+  private double amount;
 
-    public String getFrom() {
-        return from;
-    }
+  public String getFrom() {
+    return from;
+  }
 
-    public void setFrom(String from) {
-        this.from = from;
-    }
+  public void setFrom(String from) {
+    this.from = from;
+  }
 
-    public String getTo() {
-        return to;
-    }
+  public String getTo() {
+    return to;
+  }
 
-    public void setTo(String to) {
-        this.to = to;
-    }
+  public void setTo(String to) {
+    this.to = to;
+  }
 
-    public double getAmount() {
-        return amount;
-    }
+  public double getAmount() {
+    return amount;
+  }
 
-    public void setAmount(double amount) {
-        this.amount = amount;
-    }
+  public void setAmount(double amount) {
+    this.amount = amount;
+  }
 }
